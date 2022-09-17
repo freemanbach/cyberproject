e4abbcd83e2e158ccf03404edd860625332a08e7
freeman lo <flo@radford.edu>
2022-09-17 15:29:04 -0400

    initial Cyber Code

---
+++ phase1/systemapp/flo/csharp/projs/CyberApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// ITEC 325 Class Project
+//
+using System;
+using System.Text;
+
+namespace CyberApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello kids!");
+        }
+    }
+}
