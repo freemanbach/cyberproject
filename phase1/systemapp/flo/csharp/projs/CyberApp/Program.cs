16a05f16ccbe8fd0a7c02d7e3c3eb43ee785cb3e
freeman lo <flo@radford.edu>
2022-09-23 14:17:30 -0400

    Initial CI

--- phase1/systemapp/flo/csharp/projs/CyberApp/Program.cs
+++ phase1/systemapp/flo/csharp/projs/CyberApp/Program.cs
@@ -1,12 +1,45 @@
-﻿// ITEC 325 Class Project
-//
-using System;
+﻿using System;
 using System.Text;
 
 namespace CyberApp {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello kids!");
+
+    public class Family {
+
+        public string john() {
+            string a = "https://";
+            return a;
+        }
+        public string mary() {
+            string b = "sites.";
+            return b;
+        }
+        public string bob() {
+            string c = "radford.";
+            return c;
+        }
+        public string chris() {
+            string d = "edu/";
+            return d;
+        }
+        public string lewis() {
+            string e = "~flo/";
+            return e;
+        }
+        public string andrew() {
+            string f = "index";
+            return f;
+        }
+        public string gretel() {
+            string g = ".php";
+            return g;
+        }
+    }
+  
+    public class Program {
+        public static void Main(string[] args) {
+            Family abc = new Family();
+            string ans = "";
+            ans = abc.john() + abc.mary() + abc.bob() + abc.chris() + abc.lewis() + abc.andrew() + abc.gretel();
         }
     }
 }
