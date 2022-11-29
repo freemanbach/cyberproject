ec4fa50df52fb1bf1764b1f9d599e26a78607e2d
Firefoxdylan <36141875+Firefoxdylan@users.noreply.github.com>
2022-11-29 16:07:53 -0500

    Create Main.java

---
+++ phase1/dhall113/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String one = "https://";
+		String two = "sites.radford.edu";
+		String three = "/~flo";
+		String four = "/index.php";
+		String ten = one + two + three + four;
+	}
+}
