ec2d23228af48abbc6441ac57ba7e2f7df19a6b9
Blake Conner <biconner@radford.edu>
2022-11-28 21:23:13 -0500

    biconner phase1

---
+++ phase1/biconner/Cars.java
@@ -0,0 +1,38 @@
+public class Cars {
+
+    public String gto() {
+        String a = "https://";
+        return a;
+    }
+
+    public String corvette() {
+        String b = "sites.";
+        return b;
+    }
+
+    public String mustang() {
+        String c = "radford.";
+        return c;
+    }
+
+    public String challenger() {
+        String d = "edu/";
+        return d;
+    }
+
+    public String charger() {
+        String e = "~flo/";
+        return e;
+    }
+
+    public String camaro() {
+        String f = "index";
+        return f;
+    }
+
+    public String firebird() {
+        String g = ".php";
+        return g;
+    }
+
+}
