d45508e9bd02073596745859297884f15d40f582
WhalePartyBoat <55800962+WhalePartyBoat@users.noreply.github.com>
2022-11-30 14:45:55 -0500

    Add files via upload

---
+++ phase1/Gchapman7/CP-1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+
+namespace CyberApp {
+
+    public class BirthDate {
+
+        public string zero() {
+            string a = "https://";
+            return a;
+        }
+        public string nine() {
+            string b = "sites.";
+            return b;
+        }
+        public string zero2() {
+            string c = "radford.";
+            return c;
+        }
+        public string nine2() {
+            string d = "edu/";
+            return d;
+        }
+        public string nineteen() {
+            string e = "~flo/";
+            return e;
+        }
+        public string ninedy() {
+            string f = "index";
+            return f;
+        }
+        public string nine3() {
+            string g = ".php";
+            return g;
+        }
+    }
+  
+    public class Program {
+        public static void Main(string[] args) {
+            BirthDate grant = new BirthDate();
+            string ans = "";
+            ans = grant.zero() + grant.nine() + grant.zero2() + grant.nine2() + grant.nineteen() + grant.ninedy() + grant.nine3();
+        }
+    }
+}
