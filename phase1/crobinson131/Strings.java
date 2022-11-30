c6dea880cc07e00694559615ee84375c243e7d51
crobinson131 <92392926+crobinson131@users.noreply.github.com>
2022-11-30 10:43:47 -0500

    Add files via upload

---
+++ phase1/crobinson131/Strings.java
@@ -0,0 +1,31 @@
+
+public class Strings {
+	 public String str1() {
+         String a = "https://";
+         return a;
+     }
+     public String str2() {
+         String b = "sites.";
+         return b;
+     }
+     public String str3() {
+         String c = "radford.";
+         return c;
+     }
+     public String str4() {
+         String d = "edu/";
+         return d;
+     }
+     public String str5() {
+         String e = "~flo/";
+         return e;
+     }
+     public String str6() {
+         String f = "index";
+         return f;
+     }
+     public String str7() {
+         String g = ".php";
+         return g;
+     }
+}
