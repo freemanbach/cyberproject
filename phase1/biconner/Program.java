e049e07dae8bb61def22d118bce9aca569af9626
biconner <71944235+biconner@users.noreply.github.com>
2022-11-30 14:46:02 -0500

    update phase 1

--- phase1/biconner/Program.java
+++ phase1/biconner/Program.java
@@ -1,7 +1,6 @@
 public class Program {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         Cars cars = new Cars();
-        String ans = "";
-        ans = cars.gto() + cars.corvette() + cars.mustang() + cars.challenger() + cars.charger() + cars.camaro() + cars.firebird();
+        String ans = cars.gto() + cars.corvette() + cars.mustang() + cars.challenger() + cars.charger() + cars.camaro() + cars.firebird();
     }
 }
